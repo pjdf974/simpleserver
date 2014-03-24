d2b1b691ac89d10f8487994bbb694a8b604769e9
pjdf974 <p0k3pypr0g@gmail.com>
2014-03-24 08:03:06 +0400

    Modif_1
    Ajout form

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var http = require("http");
 var fs = require("fs");
 var path = require("path");
+var url = require("url");
+var mimes = require("./json/mimetypes");
 
 var IP = process.env.OPENSHIFT_NODEJS_IP;
 var PORT = process.env.OPENSHIFT_NODEJS_PORT;
 
 http.createServer(function(request, response){
 	//console.log(request.url);
-	fs.createReadStream(path.join(__dirname, "public", "index.html"))
-		.on("open", function(){
-			response.writeHead(200, {"content-type":"text/html"});
-			this.pipe(response);
-		})
-		.on("error", function(error){
-			response.writeHead(404, {"content-type":"text/plain; charset=utf8"});
-			response.end("ERREUR 404 = "+error);
-		});
+	var parsed = url.parse(request.url, true);
+	if(request.url === "/") parsed.pathname = "index.html";
+	switch(parsed.pathname){
+		case "/action":
+			response.writeHead(200, mimes.plain);
+			response.end("REPONSE SU SERVEUR = "+parsed.query.formNom+" a "+parsed.query.formAge+" ans!!!");
+			break;
+		default:
+			fs.createReadStream(path.join(__dirname, "public", parsed.pathname))
+				.on("open", function(){
+					response.writeHead(200, mimes[path.extname(parsed.pathname)]);
+					this.pipe(response);
+				})
+				.on("error", function(error){
+					response.writeHead(404, mimes.plain);
+					response.end("ERREUR 404 = "+error);
+				});
+			break;
+	};
 }).listen(PORT || 12345, IP || "localhost", function(){
 	//console.log("Server listen...");
 });
