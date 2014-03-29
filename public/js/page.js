0fe4912d230aeb8baf390cbfb9828563a40e7ef7
pjdf974 <p0k3pypr0g@gmail.com>
2014-03-29 12:14:56 +0400

    add_nedb
    Ajout du module nedb.js pour gérer les connexions

---
+++ public/js/page.js
@@ -0,0 +1,17 @@
+onload = function(){
+	var xml = new XMLHttpRequest();
+
+	xml.onload = function(){
+		var result = xml.responseText.split("\n");
+		for(var i=0, l=result.length; i<l; i++){
+			slt.add(new Option(result[i]));
+		};
+		slt.selectedIndex = slt.length-1;
+	};
+
+	btnSend.onclick = function(){
+		xml.open("get", "/getdatas?login="+txtLogin.value+"&motdepasse="+txtMotdepasse.value+"&commande="+txtCommande.value);
+		xml.send();
+	};
+
+};
--- server.js
+++ server.js
@@ -1,24 +1,68 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var url = require("url");
-var mimes = require("./json/mimetypes");
+var http   = require("http"),
+    fs     = require("fs"),
+    path   = require("path"),
+    url    = require("url"),
+    Nedb   = require("nedb"),
+    mimes  = require("./json/mimes"),
+    config = require("./json/config");
 
-var IP = process.env.OPENSHIFT_NODEJS_IP;
-var PORT = process.env.OPENSHIFT_NODEJS_PORT;
+const IP   = process.env.OPENSHIFT_NODEJS_IP || config.localhost,
+      PORT = process.env.OPENSHIFT_NODEJS_PORT || config.localport;
+
+var db = new Nedb({filename:path.join(process.cwd(), config.dirbase, config.namebase), autoload:true});
 
 http.createServer(function(request, response){
-	var parsed = url.parse(request.url);
-	if(request.url === "/") parsed.pathname = "index.html";
-	fs.createReadStream(path.join(__dirname, "public", parsed.pathname))
-		.on("open", function(){
-			response.writeHead(200, mimes[path.extname(parsed.pathname)]);
-			this.pipe(response);
-		})
-		.on("error", function(error){
-			response.writeHead(307, {"location":"error.html"});
-			response.end();
-		});
-}).listen(PORT || 12345, IP || "localhost", function(){
+	var parsed = url.parse(request.url, true);
+	if(request.url === "/") {
+		parsed.pathname = config.index;
+		db.insert({p:request.client.remotePort, a:request.client.remoteAddress, d:new Date()});
+	};
+	switch(parsed.pathname){
+		case "/getdatas":
+			if(parsed.query.motdepasse===config.motdepasse && parsed.query.login===config.login){
+				switch(parsed.query.commande){
+					case "all":
+						db.find({}, function(error, result){
+							var envoi = "";
+							for(var i=0, l=result.length; i<l; i++){
+								envoi += JSON.stringify(result[i])+"\n";
+							};
+							response.writeHead(200, mimes.plain);
+							response.end(envoi);
+						});
+						break;
+					case "delete":
+						db.remove({}, {multi:true}, function(error, nbr){
+							response.writeHead(200, mimes.plain);
+							response.end("DELETE: "+nbr+" enregistrement(s) effacé(s). La base a été entièrement vidée...");
+						});
+						break;
+					case "help":
+						response.writeHead(200, mimes.plain);
+						response.end("AIDE  : all (Afficher toutes les connexions), delete (Effacer toute la base)");
+						break;
+					default:
+						response.writeHead(200, mimes.plain);
+						response.end("ERREUR: Aucune commande saisie !");
+						break;
+				};
+			}else{
+				response.writeHead(200, mimes.plain);
+				response.end("ERREUR: Login ou mot de passe inconnu.");
+			};
+			break;
+		default:
+			fs.createReadStream(path.join(__dirname, config.static, parsed.pathname))
+				.on("open", function(){
+					response.writeHead(200, mimes[path.extname(parsed.pathname)]);
+					this.pipe(response);
+				})
+				.on("error", function(error){
+					response.writeHead(307, {"location":config.error});
+					response.end();
+				});
+			break;
+	};
+}).listen(PORT, IP, function(){
 	//console.log("Server listen...");
 });
